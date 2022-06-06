9108b1eb67c2dc6f92a8e287731f7392905e5a2f
rojodemesa <98437854+rojodemesa@users.noreply.github.com>
2022-06-06 09:44:21 +0800

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,18 @@
+print("Welcome to the Band Name Generator.")
+x = input("What\'s the name of the city you grew up in?")
+print(x)
+y = input("What is the name of your pet?")
+print(y)
+print("Your Band Name is:",x +' ' + y)
+
+
+
+
+
+
+
+
+
+
+
+
